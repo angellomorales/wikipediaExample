4bf9bddca9d3eae26e4c3e2b76b148dc2d663661
angellomorales <ing.angellomorales@gmail.com>
2020-10-16 15:59:06 -0500

    update form in layout.html to add action for search.html and added method search to view.py accomplish the requirements for search

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 from . import util
 
@@ -14,13 +14,47 @@ def entry(request, title):
     content = util.get_entry(title)
     if content == None:
         content = "Page Not Found"
-        title=content
+        title = content
         return render(request, "encyclopedia/error.html", {
-        "title": title,
-        "content":content
-    })
+            "title": title,
+            "content": content
+        })
     else:
         return render(request, "encyclopedia/entry.html", {
             "title": title,
-            "content":content
+            "content": content
         })
+
+
+def search(request):
+    entries = util.list_entries()
+    find_entries = list()
+
+    if request.method == "GET":
+        result = request.GET["q"]
+        content = util.get_entry(result)
+        if content == None:
+            content = "Page Not Found"
+            for entry in entries:
+                if result in entry:
+                    find_entries.append(entry)
+                elif result.capitalize() in entry:
+                    find_entries.append(entry)
+                elif result.upper() in entry:
+                    find_entries.append(entry)
+                elif result.lower() in entry:
+                    find_entries.append(entry)
+            if find_entries:
+                return render(request, "encyclopedia/search.html", {
+                    "search_result": find_entries,
+                })
+            else:
+                return render(request, "encyclopedia/error.html", {
+                    "title": content,
+                    "content": content
+                })
+        else:
+            return render(request, "encyclopedia/entry.html", {
+                "title": result.capitalize(),
+                "content": content
+            })
