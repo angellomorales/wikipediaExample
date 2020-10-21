b2ead78d9835baee48ccb599a409e6e4e9723690
angellomorales <ing.angellomorales@gmail.com>
2020-10-21 11:37:26 -0500

    modified get_entry function to add optionally convert markdown to html feature inside te function

--- encyclopedia/util.py
+++ encyclopedia/util.py
@@ -2,6 +2,7 @@ import re
 
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
+from markdown2 import Markdown
 
 
 def list_entries():
@@ -10,7 +11,7 @@ def list_entries():
     """
     _, filenames = default_storage.listdir("entries")
     return list(sorted(re.sub(r"\.md$", "", filename)
-                for filename in filenames if filename.endswith(".md")))
+                       for filename in filenames if filename.endswith(".md")))
 
 
 def save_entry(title, content):
@@ -25,14 +26,20 @@ def save_entry(title, content):
     default_storage.save(filename, ContentFile(content))
 
 
-def get_entry(title):
+def get_entry(title,convert):
     """
     Retrieves an encyclopedia entry by its title. If no such
     entry exists, the function returns None.
+    if exist and you want return content converted to Html
     """
     try:
         f = default_storage.open(f"entries/{title}.md")
-        return f.read().decode("utf-8")
+        content=f.read().decode("utf-8")
+        if not convert:
+            return content
+        else:
+            html=markdownToHtml(content)
+            return html
     except FileNotFoundError:
         return None
 
@@ -41,12 +48,12 @@ def checkSave(title, content):
     """
     check for create entry and save if it doesn't extis and return true.
     """
-    exist = get_entry(title)
+    exist = get_entry(title,False)
     if exist == None:
         # myfile = open(f"entries/{title.capitalize()}.md", "w")
         # myfile.write(f'{content}')
         # myfile.close
-        save_entry(title.capitalize(),content)
+        save_entry(title.capitalize(), content)
         isSaved = True
     else:
         isSaved = False
@@ -57,11 +64,19 @@ def checkEdit(title, content):
     """
     check for edit entry and save if it exist and return true.
     """
-    exist = get_entry(title)
+    exist = get_entry(title,False)
     if exist == None:
         isEdited = False
     else:
-        save_entry(title,content)
+        save_entry(title, content)
         isEdited = True
     return isEdited
 
+
+def markdownToHtml(content):
+    """
+    convert the content markdown into html format and return.
+    """
+    markdowner = Markdown()
+    html = markdowner.convert(content)
+    return html
